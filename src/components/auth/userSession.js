a2a5195f006b2db2e3a2b603af51764183799c96
Tomas <teizaguirre331@gmail.com>
2023-04-14 14:15:37 +0200

    copia de archivo auth a master

---
+++ src/components/auth/userSession.js
@@ -0,0 +1,45 @@
+// import { onAuthStateChanged } from "firebase/auth";
+// import { useState, useEffect } from "react";
+// import { auth } from "../../firebase-config";
+
+// export function useAuth() {
+//     const [user, setUser] = useState(null);
+  
+//     useEffect(() => {
+//       const unsubscribe = onAuthStateChanged(auth, (user) => {
+//         console.log(user);
+
+//         setUser(user);
+//       });
+  
+//       return unsubscribe;
+//     }, []);
+  
+//     return user;
+// }
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { auth } from "../../firebase-config";
+
+
+export function useAuth() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+
+    // Escuchar cambios en el estado de autenticación del usuario
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+
+    // Devolver una función para limpiar el listener cuando el componente se desmonte
+    return unsubscribe;
+  }, []);
+
+  return { user, isLoading };
+}
+
+
